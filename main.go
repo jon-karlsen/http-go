3dde32f42cb8c809f864513baccc96f60c16af9b
Jon Karlsen <karlsen.jon@icloud.com>
2025-11-03 13:51:27 +0900

    chore: cleanup

--- main.go
+++ main.go
@@ -30,11 +30,6 @@ func handleConn(conn net.Conn, bytesToRead int) {
 
                 line += string(buf)
         }
-
-        if len(line) > 0 {
-                fmt.Printf("read: %s\n", line)
-        }
-        return
 }
 
 func main() {
@@ -56,19 +51,4 @@ func main() {
 
                 go handleConn(conn, bytesToRead)
         }
-
-        // stream := getLinesChannel(file, bytesToRead)
-        // for line := range stream {
-        //         fmt.Printf("read: %s\n", line)
-        // }
 }
-//
-// Create a string variable to hold the contents of the "current line" of the file. It needs to persist between reads (loop iterations).
-// After reading 8 bytes, split the data on newlines (\n) to create a slice of strings - let's call these split sections "parts". There will typically only be one or two "parts" because we're only reading 8 bytes at a time.
-// For each part except the last one, print a line to the console in this format:
-// read: LINE
-//
-// Where LINE is the "current line" we've aggregated so far plus the current "part". Then reset the "current line" variable to an empty string. Note that if we only have one "part", we don't need to print, as we have not reached a new line yet.
-//
-// Add the last "part" to the "current line" variable. Repeat until you reach the end of the file.
-// Once you're done reading the file, if there's anything left in the "current line" variable, print it in the same read: LINE format.

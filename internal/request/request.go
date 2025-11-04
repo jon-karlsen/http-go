ceb3ef2beb8027bf5ec9b67d2978fea191f93566
Jon Karlsen <karlsen.jon@icloud.com>
2025-11-04 18:01:33 +0900

    c4l2

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,113 @@
 package request
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type RequestLine struct {
+        HttpVersion     string
+        RequestTarget   string
+        Method          string
+}
+
+type Request struct {
+        RequestLine RequestLine
+}
+
+var ERR_BAD_NO_PARTS = fmt.Errorf("Error parsing request line; invalid number of parts")
+var SEPARATOR = "\r\n"
+
+func parseRequestMethod(input *string) (*string, error) {
+        for _, ch := range(*input) {
+                if ch != ' ' {
+                        if int(ch) < int('A') || int(ch) > int('Z') {
+                                return nil, fmt.Errorf("Invalid HTTP method: '%s'", *input)
+                        }
+                }
+        }
+
+        return input, nil
+}
+
+func parseRequestVersion(input *string) (*string, error) {
+        parts := strings.Split(*input, "/")
+        if parts[len(parts)-1] != "1.1" {
+                return nil, fmt.Errorf("Invalid version '%s'; only 'HTTP/1.1' is supported", *input)
+        }
+
+        return &parts[len(parts)-1], nil
+}
+
+func parseToArrOfStrings(input []byte) ([]string, []byte, error) {
+        const expectedParts = 3
+
+        i, j, n := 0, 0, len(input)
+        output := make([]string, expectedParts, expectedParts)
+
+        for i < n {
+                if i+1 < n && string(input[i:i+len(SEPARATOR)]) == SEPARATOR {
+                        break
+                }
+
+                if input[i] == ' ' {
+                        j++
+                        if j >= len(output) {
+                                return nil, nil, ERR_BAD_NO_PARTS
+                        }
+                } else {
+                        output[j] += string(input[i])
+                }
+                i++
+        }
+
+        if j < expectedParts-1 || output[j] == "" {
+                return nil, nil, ERR_BAD_NO_PARTS
+        }
+
+        return output, input[i+len(SEPARATOR):], nil
+}
+
+func parseRequestLine(input []byte) (*RequestLine, []byte, error) {
+        parsed, rem, err := parseToArrOfStrings(input)
+        if err != nil {
+                return nil, nil, err
+        }
+
+        method, err := parseRequestMethod(&parsed[0])
+        if err != nil {
+                return nil, nil, err
+        }
+
+        ver, err := parseRequestVersion(&parsed[2])
+        if err != nil {
+                return nil, nil, err
+        }
+
+        output := RequestLine{
+                HttpVersion: *ver,
+                RequestTarget: parsed[1],
+                Method: *method,
+        }
+
+        return &output, rem, nil
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+        input, err := io.ReadAll(reader)
+        if err != nil {
+                return nil, err
+        }
+
+        reqLine, _, err := parseRequestLine(input)
+        if err != nil {
+                return nil, err
+        }
+
+        output := Request{
+                RequestLine: *reqLine,
+        }
+
+        return &output, nil
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,51 @@
 package request
 
 import (
+        "strings"
         "testing"
 
         "github.com/stretchr/testify/assert"
+        "github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-        assert.Equal(t, "TheTestagen", "TheTestagen")
+        // TEST: Good GET request line
+        test_input := "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"
+        r, err := RequestFromReader(strings.NewReader(test_input))
+
+        require.NoError(t, err)
+        require.NotNil(t, r)
+
+        assert.Equal(t, "GET", r.RequestLine.Method)
+        assert.Equal(t, "/", r.RequestLine.RequestTarget)
+        assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+        // TEST: Good GET Request line with path
+        test_input = "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"
+        r, err = RequestFromReader(strings.NewReader(test_input))
+
+        require.NoError(t, err)
+        require.NotNil(t, r)
+
+        assert.Equal(t, "GET", r.RequestLine.Method)
+        assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+        assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+        // TEST: Invalid number of parts in request line (too few)
+        test_input = "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"
+        _, err = RequestFromReader(strings.NewReader(test_input))
+        require.Error(t, err)
+        assert.EqualError(t, err, "Error parsing request line; invalid number of parts")
+
+        // TEST: Invalid number of parts in request line (too many)
+        test_input = "GET /coffee latte HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"
+        _, err = RequestFromReader(strings.NewReader(test_input))
+        require.Error(t, err)
+        assert.EqualError(t, err, "Error parsing request line; invalid number of parts")
+
+        // TEST: Invalid version in Request line
+        test_input = "GET / HTTP/1.2\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"
+        r, err = RequestFromReader(strings.NewReader(test_input))
+        require.Error(t, err)
+        assert.EqualError(t, err, "Invalid version 'HTTP/1.2'; only 'HTTP/1.1' is supported")
 }
